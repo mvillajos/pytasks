4e8d95e3e85a86f183fcc16ac0ff7ec48d272611
mvillajos <mvillajos@outlook.com>
2023-08-26 23:38:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from routes.task import task
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
+
+app = FastAPI()
+origins = [config('FRONTEND_URL')]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+@app.get('/')
+def welcome():
+    return {'message': 'Welcome to my FastAPI API'}
+
+
+app.include_router(task)
+
---
+++ models.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field 
+from typing import Optional
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid ObjectId')
+        return str(v)
+
+class Task(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    title: str
+    description: Optional[str] = None
+    completed: bool=False
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        #arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+
+class UpdateTask(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    completed: Optional[bool] = None
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        json_encoders = {ObjectId: str}
